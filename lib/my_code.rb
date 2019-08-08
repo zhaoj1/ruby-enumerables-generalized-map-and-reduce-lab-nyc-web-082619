23aaf4b27266c5828080ed7cb81a3c81a586acb5
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-08 05:24:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,7 +18,7 @@ def reduce(array, starting_point = nil)
     total = array[0]
     i = 1
   end
-  while i < array.length 
+  while i < array.length do
     total = yield(total, array[i])
     i += 1
   end
