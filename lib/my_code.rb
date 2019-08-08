d55e8a83516e56d14abf3439e98913837f690f46
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-08 05:26:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,13 +11,8 @@ def map(array)
 end
 
 def reduce(array, starting_point = nil)
-  if starting_point 
     total = starting_point
     i = 0
-  else
-    total = array[0]
-    i = 1
-  end
   while i < array.length do
     total = yield(total, array[i])
     i += 1
