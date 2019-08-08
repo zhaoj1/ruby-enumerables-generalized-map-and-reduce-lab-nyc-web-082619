27d9bd84dbfd4d1a4975193c0b7ae3f6914666b4
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-08 05:34:11 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
 # Your Code Here
+
+def map(array)
+  new = []
+  i = 0
+  while i < array.length do
+    new.push(yield(array[i]))
+    i += 1
+  end
+  return new
+end
+
+def reduce(array, starting_point = nil)
+  if starting_point 
+    total = starting_point
+    i = 0
+  else
+    total = array[0]
+    i = 1
+  end
+  while i < array.length do
+    total = yield(total, array[i])
+    i += 1
+  end
+  total
+end  
